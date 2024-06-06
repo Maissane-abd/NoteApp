8406179148cf991bae7ab31fb7585cb53261ca6e
Maïssane Abdallah <abd.maissane@gmail.com>
2024-06-06 18:34:27 +0200

    some comments

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -12,19 +12,31 @@ export default function NotesList() {
 
     // Rendu du composant NotesList
     // Pour l'instant, il retourne simplement un div avec le texte "NotesList"
-    return (
-        <div className='p-10 w-full'>
-            <p className='text-xl text-slate-200 mb-6 '>
-                Bienvenue sur votre NoteApp
-            </p>
-            <div className='grid lg:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-5 gap-6'>
-                {notes.list?.length > 0 && notes.list.map(note => (
-                    <div className='bg-slate-100 hover:bg-slate-50 p-4 rounded cursor-pointer'>
+    // Rendu de l'interface utilisateur
+return (
+    <div className='p-10 w-full'>
+        <p className='text-xl text-slate-200 mb-6'>
+            Bienvenue sur votre NoteApp
+        </p>
+
+        {/* Div pour afficher les notes */}
+        <div className='grid lg:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-5 gap-6'>
+           
+            {/* Vérification de la présence de notes et affichage conditionnel */}
+            {notes.list?.length > 0 && notes.list.map(note => (
+              
+              // Div pour chaque note avec des classes CSS pour le style
+                <div className='bg-slate-100 hover:bg-slate-50 p-4 rounded cursor-pointer'>
+                
+                    {/* Titre de la note */}
                     <p className='text-lg font-semibold'>{note.title}</p>
+                
+                    {/* Sous-titre de la note */}
                     <p className='text-gray-700'>{note.subtitle}</p>
-                    </div>
-                ))}
-            </div> 
-        </div>
-    )
+                </div>
+            ))}
+        </div> 
+    </div>
+);
+
 }
